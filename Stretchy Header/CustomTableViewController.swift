83c19bb4a58ad6f3bd064140342945f6a43452df
Rohit Nikam <rohit.nikam@hindavi.in>
2020-09-28 16:08:21 +0530

    Second Commit.

--- Stretchy Header/CustomTableViewController.swift
+++ Stretchy Header/CustomTableViewController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomTableViewController: UITableViewController {
+    
+    var imageView: UIImageView? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.contentInset = UIEdgeInsets(top: 250, left: 0, bottom: 0, right: 0)
+    }
+}
+
+extension CustomTableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hindavi Technologies"
+        return cell
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = -scrollView.contentOffset.y
+        let height = max(y, 120)
+        imageView?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: height)
+    }
+}
--- Stretchy Header/ViewController.swift
+++ Stretchy Header/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myTable = CustomTableViewController()
+        self.addChild(myTable)
+        view.addSubview(myTable.tableView)
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "HindaviLogo")
+        imageView.backgroundColor = .green
+        imageView.contentMode = .center
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 250)
+        view.addSubview(imageView)
+        
+        myTable.imageView = imageView
     }
-
 
 }
 
